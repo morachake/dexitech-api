acc21628c2b45b87d05f55fd83fa10d3c9cb2504
morachake <jacobmoracha@gmail.com>
2024-11-05 22:49:43 +0300

    Fixing api docs error for swagger

--- users/serializers.py
+++ users/serializers.py
@@ -6,6 +6,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
     email = serializers.EmailField(source='user.email', read_only=True)
     
+    
     class Meta:
         model = UserProfile
         fields = ['id', 'username', 'email', 'phone_number', 'address', 'created_at']
